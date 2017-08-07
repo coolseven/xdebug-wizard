94221c60a237c772b8d8efc137acbba3aa8ed999
coolseven <coolseven2013@gmail.com>
2017-08-08 01:54:06 +0800

    add static modifier on PageController's app() method ,to remind me that controller should be stateless

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,15 +3,29 @@
 namespace App\Http\Controllers;
 
 
+use Closure;
+use Illuminate\Support\Facades\Cache;
+
 class PageController extends Controller
 {
+    /**
+     * Controller-Scoped Middleware
+     */
+    public function __construct()
+    {
+        $this->middleware(function($request , Closure $next){
+            return $next($request);
+        });
+    }
+
     /**
      * page requests
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function app()
+    public static function app(Cache $cache)
     {
+        dd($cache);
         return view('app');
     }
 }
